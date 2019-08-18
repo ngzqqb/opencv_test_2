ce54100f5cec97a1afd35ecc2817f51845a17587
ngzHappy <zhaixueqiang@hotmail.com>
2019-08-18 16:42:55 +0800

    +update

---
+++ StaticOpenCV/staticlib/OpenCVModules-debug.cmake
@@ -0,0 +1,229 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Debug".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "zlib" for configuration "Debug"
+set_property(TARGET zlib APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(zlib PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc15/staticlib/zlibd.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS zlib )
+list(APPEND _IMPORT_CHECK_FILES_FOR_zlib "${_IMPORT_PREFIX}/x64/vc15/staticlib/zlibd.lib" )
+
+# Import target "libjpeg-turbo" for configuration "Debug"
+set_property(TARGET libjpeg-turbo APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(libjpeg-turbo PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc15/staticlib/libjpeg-turbod.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS libjpeg-turbo )
+list(APPEND _IMPORT_CHECK_FILES_FOR_libjpeg-turbo "${_IMPORT_PREFIX}/x64/vc15/staticlib/libjpeg-turbod.lib" )
+
+# Import target "libtiff" for configuration "Debug"
+set_property(TARGET libtiff APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(libtiff PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C;CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc15/staticlib/libtiffd.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS libtiff )
+list(APPEND _IMPORT_CHECK_FILES_FOR_libtiff "${_IMPORT_PREFIX}/x64/vc15/staticlib/libtiffd.lib" )
+
+# Import target "libwebp" for configuration "Debug"
+set_property(TARGET libwebp APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(libwebp PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc15/staticlib/libwebpd.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS libwebp )
+list(APPEND _IMPORT_CHECK_FILES_FOR_libwebp "${_IMPORT_PREFIX}/x64/vc15/staticlib/libwebpd.lib" )
+
+# Import target "libpng" for configuration "Debug"
+set_property(TARGET libpng APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(libpng PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc15/staticlib/libpngd.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS libpng )
+list(APPEND _IMPORT_CHECK_FILES_FOR_libpng "${_IMPORT_PREFIX}/x64/vc15/staticlib/libpngd.lib" )
+
+# Import target "IlmImf" for configuration "Debug"
+set_property(TARGET IlmImf APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(IlmImf PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc15/staticlib/IlmImfd.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS IlmImf )
+list(APPEND _IMPORT_CHECK_FILES_FOR_IlmImf "${_IMPORT_PREFIX}/x64/vc15/staticlib/IlmImfd.lib" )
+
+# Import target "libprotobuf" for configuration "Debug"
+set_property(TARGET libprotobuf APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(libprotobuf PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc15/staticlib/libprotobufd.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS libprotobuf )
+list(APPEND _IMPORT_CHECK_FILES_FOR_libprotobuf "${_IMPORT_PREFIX}/x64/vc15/staticlib/libprotobufd.lib" )
+
+# Import target "quirc" for configuration "Debug"
+set_property(TARGET quirc APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(quirc PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "C"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc15/staticlib/quircd.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS quirc )
+list(APPEND _IMPORT_CHECK_FILES_FOR_quirc "${_IMPORT_PREFIX}/x64/vc15/staticlib/quircd.lib" )
+
+# Import target "ade" for configuration "Debug"
+set_property(TARGET ade APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(ade PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc15/staticlib/ade.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ade )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ade "${_IMPORT_PREFIX}/x64/vc15/staticlib/ade.lib" )
+
+# Import target "opencv_core" for configuration "Debug"
+set_property(TARGET opencv_core APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_core PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc15/staticlib/opencv_core411d.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_core )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_core "${_IMPORT_PREFIX}/x64/vc15/staticlib/opencv_core411d.lib" )
+
+# Import target "opencv_flann" for configuration "Debug"
+set_property(TARGET opencv_flann APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_flann PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc15/staticlib/opencv_flann411d.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_flann )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_flann "${_IMPORT_PREFIX}/x64/vc15/staticlib/opencv_flann411d.lib" )
+
+# Import target "opencv_imgproc" for configuration "Debug"
+set_property(TARGET opencv_imgproc APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_imgproc PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc15/staticlib/opencv_imgproc411d.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_imgproc )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_imgproc "${_IMPORT_PREFIX}/x64/vc15/staticlib/opencv_imgproc411d.lib" )
+
+# Import target "opencv_ml" for configuration "Debug"
+set_property(TARGET opencv_ml APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_ml PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc15/staticlib/opencv_ml411d.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_ml )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_ml "${_IMPORT_PREFIX}/x64/vc15/staticlib/opencv_ml411d.lib" )
+
+# Import target "opencv_photo" for configuration "Debug"
+set_property(TARGET opencv_photo APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_photo PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc15/staticlib/opencv_photo411d.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_photo )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_photo "${_IMPORT_PREFIX}/x64/vc15/staticlib/opencv_photo411d.lib" )
+
+# Import target "opencv_dnn" for configuration "Debug"
+set_property(TARGET opencv_dnn APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_dnn PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc15/staticlib/opencv_dnn411d.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_dnn )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_dnn "${_IMPORT_PREFIX}/x64/vc15/staticlib/opencv_dnn411d.lib" )
+
+# Import target "opencv_features2d" for configuration "Debug"
+set_property(TARGET opencv_features2d APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_features2d PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc15/staticlib/opencv_features2d411d.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_features2d )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_features2d "${_IMPORT_PREFIX}/x64/vc15/staticlib/opencv_features2d411d.lib" )
+
+# Import target "opencv_gapi" for configuration "Debug"
+set_property(TARGET opencv_gapi APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_gapi PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc15/staticlib/opencv_gapi411d.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_gapi )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_gapi "${_IMPORT_PREFIX}/x64/vc15/staticlib/opencv_gapi411d.lib" )
+
+# Import target "opencv_imgcodecs" for configuration "Debug"
+set_property(TARGET opencv_imgcodecs APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_imgcodecs PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc15/staticlib/opencv_imgcodecs411d.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_imgcodecs )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_imgcodecs "${_IMPORT_PREFIX}/x64/vc15/staticlib/opencv_imgcodecs411d.lib" )
+
+# Import target "opencv_calib3d" for configuration "Debug"
+set_property(TARGET opencv_calib3d APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_calib3d PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc15/staticlib/opencv_calib3d411d.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_calib3d )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_calib3d "${_IMPORT_PREFIX}/x64/vc15/staticlib/opencv_calib3d411d.lib" )
+
+# Import target "opencv_highgui" for configuration "Debug"
+set_property(TARGET opencv_highgui APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_highgui PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc15/staticlib/opencv_highgui411d.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_highgui )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_highgui "${_IMPORT_PREFIX}/x64/vc15/staticlib/opencv_highgui411d.lib" )
+
+# Import target "opencv_objdetect" for configuration "Debug"
+set_property(TARGET opencv_objdetect APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_objdetect PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc15/staticlib/opencv_objdetect411d.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_objdetect )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_objdetect "${_IMPORT_PREFIX}/x64/vc15/staticlib/opencv_objdetect411d.lib" )
+
+# Import target "opencv_stitching" for configuration "Debug"
+set_property(TARGET opencv_stitching APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(opencv_stitching PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_DEBUG "CXX"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/x64/vc15/staticlib/opencv_stitching411d.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS opencv_stitching )
+list(APPEND _IMPORT_CHECK_FILES_FOR_opencv_stitching "${_IMPORT_PREFIX}/x64/vc15/staticlib/opencv_stitching411d.lib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
